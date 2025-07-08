ae608dc7220bd16cdb40202037415478c6a8784f
Zer40r4 <wale.ajisebutu77@gmail.com>
2025-07-08 09:42:46 -0700

    test upload

---
+++ code.py
@@ -0,0 +1,11 @@
+
+def form_tri(w):
+    return w * (w+1) // 2
+def form_pent(w):
+    return w * (3*w-1) // 2
+def form_hex(w):
+    return w * (2*w-1) // 2
+
+for n in range (285, 10000):
+    
+   
